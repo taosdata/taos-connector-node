2b1128cf23556d3e55c634894ced46f6f1adf8ba
xleili <xlli@taosdata.com>
2022-07-23 19:59:51 +0800

    test(driver):nodejs 3.0 fix ci failed on node 10.x

--- nodejs/nodetaos/globalfunc.js
+++ nodejs/nodetaos/globalfunc.js
@@ -12,3 +12,4 @@ function toTaosTSString(date) {
   return tsArr[0] + " " + tsArr[1].substring(0, tsArr[1].length-1);
 }
 global.toTaosTSString = toTaosTSString;
+global.TextEncoder = require("util").TextEncoder; 

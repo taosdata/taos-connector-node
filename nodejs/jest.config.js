fa83100d0ae032a61f5a3226d184d6c1760da838
xleili <xlli@taosdata.com>
2022-07-25 16:24:22 +0800

    test(driver):fix nodejs3.0 ci failed on node 10.x 10th

--- nodejs/jest.config.js
+++ nodejs/jest.config.js
@@ -137,7 +137,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jest-environment-node",
+  // testEnvironment: "jest-environment-node",
   // testEnvironment: "<rootDir>/test/jest_setup.js",
 
   // Options that will be passed to the testEnvironment

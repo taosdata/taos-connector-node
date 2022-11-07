2f3c86bdf530bd1a166520209daec775a80f9315
xleili <xlli@taosdata.com>
2022-11-07 17:40:21 +0800

    enh(driver):nodejs websocket add Blob in on.Message()

--- WebSocket/test/bulkPulling/version.test.ts
+++ WebSocket/test/bulkPulling/version.test.ts
@@ -1,7 +1,7 @@
-import { TDWebSocket } from "../../index"
+import { connect } from "../../index"
 const DSN = 'ws://root:taosdata@127.0.0.1:6041/rest/ws'
 // const DSN = 'ws://root:taosdata@182.92.127.131:6041/rest/ws'
-var ws = TDWebSocket(DSN)
+var ws = connect(DSN)
 
 describe('TDWebSocket.version()', () => {
     test('check version', () => {

4664e7ecc49ac881049e1a5efbf970891c90f3bc
menshibin <men_shi_bin@163.com>
2025-09-12 17:44:23 +0800

    feat: modify stmt2 test case error

--- nodejs/test/bulkPulling/stmt2.func.test.ts
+++ nodejs/test/bulkPulling/stmt2.func.test.ts
@@ -3,21 +3,19 @@ import { WSConfig } from "../../src/common/config";
 import { setLevel } from "../../src/common/log";
 import { WsSql } from "../../src/sql/wsSql";
 import { WsStmt2 } from "../../src/stmt/wsStmt2";
-import { Sleep } from "../utils";
 
-let dns = 'ws://localhost:6041'
+let dns = 'ws://192.168.2.156:6041'
 setLevel("debug")
 beforeAll(async () => {
     let conf :WSConfig = new WSConfig(dns);
     conf.setUser('root');
     conf.setPwd('taosdata');
     let wsSql = await WsSql.open(conf);
-    await wsSql.exec('drop database if exists power_func_stmt2;');
     await wsSql.exec('create database if not exists power_func_stmt2 KEEP 3650 DURATION 10 BUFFER 16 WAL_LEVEL 1;');
     await wsSql.exec('CREATE STABLE if not exists power_func_stmt2.meters (ts timestamp, current float, voltage int, phase float) TAGS (location binary(64), groupId int);');
     await wsSql.exec('CREATE STABLE if not exists power_func_stmt2.query_meters (ts timestamp, current float, voltage int, phase float) TAGS (location binary(64), groupId int);');
     let insertQuery = "INSERT INTO " +
-                "power_func_stmt2.d1001 USING power_func_stmt2.query_meters TAGS('California.SanFrancisco', 1) " +
+                "power_func_stmt2.q1001 USING power_func_stmt2.query_meters TAGS('California.SanFrancisco', 1) " +
                 "VALUES " +
                 "('2024-12-19 19:12:45.642', 50.30000, 201, 0.31000) " +
                 "('2024-12-19 19:12:46.642', 82.60000, 202, 0.33000) " +
@@ -28,7 +26,7 @@ beforeAll(async () => {
                 "('2024-12-19 17:12:45.642', 50.30000, 201, 0.31000) " +
                 "('2024-12-19 17:12:46.642', 82.60000, 202, 0.33000) " +
                 "('2024-12-19 17:12:47.642', 92.30000, 203, 0.31000) " +
-                "power_func_stmt2.d1002 USING power_func_stmt2.query_meters TAGS('Alabama.Montgomery', 2) " +
+                "power_func_stmt2.q1002 USING power_func_stmt2.query_meters TAGS('Alabama.Montgomery', 2) " +
                 "VALUES " +
                 "('2024-12-19 19:12:45.642', 50.30000, 204, 0.25000) " +
                 "('2024-12-19 19:12:46.642', 62.60000, 205, 0.33000) " +
@@ -198,7 +196,7 @@ describe('TDWebSocket.Stmt()', () => {
             dataParams.setFloat(multi[1])
             dataParams.setInt(multi[2])
             dataParams.setFloat(multi[3])
-            dataParams.setVarchar(['SanFrancisco_1', 'SanFrancisco_2', 'SanFrancisco_3']);
+            dataParams.setVarchar(['SanFrancisco_1', 'SanFrancisco_2','SanFrancisco_3']);
             dataParams.setInt([1, 2, 3]);
             await stmt.bind(dataParams)
             multi[0][0] = lastTs + 1;
@@ -299,6 +297,9 @@ describe('TDWebSocket.Stmt()', () => {
         conf.setPwd('taosdata')
         conf.setDb('power_func_stmt2')
         let connector = await WsSql.open(conf) 
+
+
+
 
         let stmt = await connector.stmtInit()
         expect(stmt).toBeTruthy() 
